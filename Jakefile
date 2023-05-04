6a7f49b76175162b6411d980bd0b8a0000ed9cef
Cube Drone <curtis@lassam.net>
2023-05-04 14:17:42 -0500

    Initial commit

---
+++ Jakefile
@@ -0,0 +1,95 @@
+let { task, desc } = require('jake');
+let { run, runBg, pipe } = require('@cube-drone/rundmc');
+
+desc("List all tools & options.")
+task('default', async () => {
+    return run("npx jake -T")
+});
+
+const setup = async () => {
+    let {setup} = require('./index')
+    await setup({
+        nodeEnv: process.env.NODE_ENV || "development",
+        redisUrl: process.env.TEMPL8_REDIS_URL || process.env.REDIS_URL ||
+            "redis://localhost:6379",
+        postgresConnectionString: process.env.TEMPL8_POSTGRES_URL || process.env.POSTGRES_URL ||
+            "postgres://postgres:example@localhost:5432/templ8",
+    })
+}
+
+desc("Build the database")
+task('setup', setup)
+
+const start = async () => {
+    await run("docker-compose up -d")
+    await setup()
+    await run("nodemon run.js")
+}
+desc("Boot up the server.")
+task('start', start)
+
+desc("unbootup the server")
+task('clean', async () => {
+    await run("docker-compose down")
+})
+
+desc("load local secrets")
+task('secrets', async () => {
+    console.log("run 'source .secrets.sh' to load local secrets")
+})
+
+desc("run tests")
+task('test', async () => {
+    await run('npx mocha')
+})
+
+const cleanTest = async () => {
+    await run("docker-compose down")
+    await run("docker-compose up -d")
+    await setup()
+    let proc = runBg("node run.js")
+
+    let success = false
+    let messages = []
+    try{
+        let tests = await pipe("npx mocha")
+        console.warn("-----------------")
+        for(let line of tests){
+            console.log(line)
+        }
+        success = true
+    }
+    catch(err){
+        console.log("Error running tests");
+        for(let line of err){
+            console.error(line)
+            if(line.indexOf && line.indexOf("failing") > -1){
+                messages.push(line)
+            }
+        }
+    }
+
+    if(messages){
+        console.error("")
+        for(let message of messages){
+            console.error(`==> ${message}`)
+        }
+        console.error("")
+    }
+
+    await proc.kill()
+    return { success, messages }
+}
+
+const ci_test = async () => {
+    let { success, messages } = await cleanTest()
+    if(!success){
+        console.error("Tests failed, not deploying")
+        return process.exit(1)
+    }
+    else{
+        return process.exit(0)
+    }
+}
+desc("Run the test suite from clean")
+task('ci_test', ci_test)
---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express')
+const crypto = require('crypto')
+require('express-async-errors') // this patches better async error handling into express
+const app = express()
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
+const assert = require('assert')
+
+const { Redis } = require("ioredis")
+
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+//--------------------------
+async function main({nodeEnv, envPort, cookieSecret, redisUrl, postgresConnectionString}){
+    app.use(cookieParser(cookieSecret))
+
+    // we are going to deploy this behind nginx
+    app.set('trust proxy', true)
+    // log stuff
+    app.use(morgan('tiny'))
+
+    const sqlDatabase = require('knex')({
+        client: 'pg',
+        connection: postgresConnectionString
+    });  
+    const redis = new Redis(redisUrl);
+
+    let noopMiddleware = async (req, res, next) => {
+        next()
+    }
+
+    // error handler
+    app.use((err, req, res, next) => {
+        console.error(err.stack)
+        let errorMessage = err.message;
+        if(nodeEnv === "production"){
+            errorMessage = "Internal Server Error"
+        }
+        res.status(500).send(errorMessage)
+    })
+
+    //---------------------------
+
+    app.get('/ping', function (req, res) {
+        res.send('pong')
+    })
+
+    app.get('/test', async function (req, res) {
+        await redis.set("ponk", "toots ahoy", "EX", 60*60*24*7)
+        let pong = await redis.get("ponk")
+        assert.strictEqual(pong, "toots ahoy")
+
+        await sqlDatabase('table_1').insert({
+            id: crypto.randomUUID(),
+            name: "toots ahoy"
+        })
+        res.send(":)")
+    })
+
+    app.listen(envPort)
+    console.log(`Listening on port ${envPort}...`)
+}
+
+async function setup({nodeEnv, envPort, redisUrl, postgresConnectionString}){
+    /*
+        this is run once, during a deploy
+    */
+    const redis = new Redis(redisUrl);
+
+    let lock = await redis.set("setup-lock", "1", "NX", "EX", 30)
+    if(!lock){
+        return;
+    }
+
+    let {connectAndSetup} = require('./database-setup')
+    let sqlDatabase = await connectAndSetup({postgresConnectionString})
+
+    console.log(`\trunning migrations...`);
+    await sqlDatabase.migrate.latest({
+        directory: './migrations',
+    })
+    let currentVersion = await sqlDatabase.migrate.currentVersion();
+    console.warn(`\tcurrent version: ${currentVersion}`);
+}
+
+module.exports = {
+    main,
+    setup
+}
